14b3d31a53faeb1c4e8f036d80c34dcbf0269dce
3milychu <3milychu@gmail.com>
2018-08-05 09:01:58 -0400

    push

--- script.py
+++ script.py
@@ -1,30 +1,64 @@
+# Get list of images from an API
+
+import requests
+api_url = 'http://127.0.0.1:5000/api/all'
+r = requests.get(api_url)
+print(r.status_code)
+data_json= r.json()
+pd.DataFrame(data_json).to_csv('data.csv', index=False)
+
+
 # Get list of images from folder and save as csv
 
 import os
 import numpy as np
 
 file_list = []
-file_path = "/Volumes/Samsung_t3/pics"
+file_path = "/Volumes/Samsung_t3/project-repos/exercises/pics"
 for root, dirs, files in os.walk(file_path):
-	for file in files:
-		file_list.append(file)
+    for file in files:
+        file_list.append(file)
+        
+print(len(file_list))
+        
+np.savetxt('file.csv', file_list, fmt="%s", delimiter = ",")
 
-np.savetxt('file_list.csv', file_list, fmt="%s", delimiter",")
 
-
-# Read from CSV and conver to JSON
+# Read from CSV 
 
 import numpy as np
-import pandas as pd 
-from matplotlib import pyplot as plt 
+import pandas as pd
+from matplotlib import pyplot as plt
 
-file_path = '/Volumes/Samsung_t3/pics/file_list.csv'
-df = pd.read_csv(path)
+file_path = "/Volumes/Samsung_t3/project-repos/exercises/file.csv"
+df = pd.read_csv(file_path)
+
+
+# Check the data and adjust/add new information as needed
 
 print(df.head())
-print(df.shape)
+df.columns=["img_name"]
+df['Index'] = df.index
+print(df.head())
 
-df['Index'] = df.indexn
+# Save new dataframe as csv
+
+df.to_csv(file_path, index=False)
+
+# Convert to json
+list(df)
+
+import csv
+import json
+
+csvfile = open(file_path, 'r')
+jsonfile = open("/Volumes/Samsung_t3/project-repos/exercises/file.json", 'w')
+
+fieldnames = ('img_name', 'Index')
+
+reader = csv.DictReader(csvfile, fieldnames)
+out = json.dumps( [row for row in reader])
+jsonfile.write(out)
 
 
 
